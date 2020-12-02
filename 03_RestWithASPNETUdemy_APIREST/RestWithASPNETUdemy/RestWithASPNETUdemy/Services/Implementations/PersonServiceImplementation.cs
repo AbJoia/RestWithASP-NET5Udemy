1ccd37952a9926426d3954420e6ec574f9a45658
Abner <abnerjoia1991@gmail.com>
2020-12-01 23:34:33 -0300

    Persistencia de dados no banco

--- 03_RestWithASPNETUdemy_APIREST/RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/PersonServiceImplementation.cs
+++ 03_RestWithASPNETUdemy_APIREST/RestWithASPNETUdemy/RestWithASPNETUdemy/Services/Implementations/PersonServiceImplementation.cs
@@ -17,36 +17,73 @@ namespace RestWithASPNETUdemy.Services.Implementations
             _context = context;
         }
 
-        public Person Create(Person person)
-        {
-           return person;
-        }
-
-        public void Delete(long id)
-        {
-           
-        }
-
         public List<Person> FindAll()
         {
             return _context.Persons.ToList();
-        }       
+        }
 
         public Person FindById(long id)
         {
-            return new Person
+            return _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+        }
+
+        public Person Create(Person person)
+        {
+            try
             {
-                Id = 1,
-                FirstName = "Abner",
-                LastName = "Jóia",
-                Address = "Rua da Silva, 65",
-                Gender = "Male",
-            };
+                _context.Add(person);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+           return person;
         }
 
         public Person Update(Person person)
         {
+            if (!Exist(person.Id)) return new Person();
+
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
+
+            if (result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+
             return person;
-        }            
+        }
+
+        public void Delete(long id)
+        {
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+            if(result != null)
+            {
+                try
+                {
+                    _context.Persons.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+
+        }         
+
+        private bool Exist(long id)
+        {
+            return _context.Persons.Any(p => p.Id.Equals(id));
+        }
     } 
 }
