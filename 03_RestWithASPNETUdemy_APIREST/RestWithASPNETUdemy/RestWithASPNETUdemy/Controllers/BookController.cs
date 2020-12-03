3025d05640646c767c8ac1c1c3408b948159972a
Abner <abnerjoia1991@gmail.com>
2020-12-03 11:10:19 -0300

    incluindo crud Book

---
+++ 03_RestWithASPNETUdemy_APIREST/RestWithASPNETUdemy/RestWithASPNETUdemy/Controllers/BookController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RestWithASPNETUdemy.Business;
+using RestWithASPNETUdemy.Model;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [ApiVersion("1")]
+    [ApiController]
+    [Route("api/[controller]/v{version:apiversion}")]
+    public class BookController : ControllerBase
+    {
+        private readonly ILogger<BookController> _logger; 
+        private IBookBusiness _bookBusiness;
+
+        public BookController(ILogger<BookController> logger, IBookBusiness bookBusiness)
+        {
+            _logger = logger;
+            _bookBusiness = bookBusiness;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_bookBusiness.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetId(long id)
+        {
+            var result = _bookBusiness.FindById(id);
+            if (result == null) return BadRequest("Livro não encontrado com o ID: " + id);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Book book)
+        {
+            if (book == null) return BadRequest();
+            return Ok(_bookBusiness.Create(book));
+        }
+
+        [HttpPut]
+        public IActionResult Put([FromBody] Book book)
+        {
+            if (book == null) return BadRequest();
+            return Ok(_bookBusiness.Update(book));
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            _bookBusiness.Delete(id);
+            return NoContent();
+        }
+    }
+}
--- 03_RestWithASPNETUdemy_APIREST/RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
+++ 03_RestWithASPNETUdemy_APIREST/RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
@@ -46,6 +46,8 @@ namespace RestWithASPNETUdemy
 
             services.AddScoped<IPersonBusiness, PersonBusinessImplementation>();
             services.AddScoped<IPersonRepository, PersonRepositoryImplementation>();
+            services.AddScoped<IBookBusiness, BookBusinessImplementation>();
+            services.AddScoped<IBookRepository, BookRepositoryImplementation>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
