05faf0bb0c1c88809de732634391c15b9b6f7bdf
Abner <abnerjoia1991@gmail.com>
2020-12-03 21:55:54 -0300

    Exemplo Custom Serialization

---
+++ 04_RestWithASPNETUdemy_APIREST-CustomSerialization/RestWithASPNETUdemy/RestWithASPNETUdemy/Data/VO/PersonVO.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Text.Json.Serialization;
+
+namespace RestWithASPNETUdemy.Data.VO
+{
+    public class PersonVO
+    {
+        [JsonPropertyName("Identificador")]
+        public long Id { get; set; }
+        [JsonPropertyName("Nome")]
+        public string FirstName { get; set; }
+        [JsonPropertyName("Sobrenome")]
+        public string LastName { get; set; }
+        [JsonPropertyName("Endereço")]
+        public string Address { get; set; }
+        [JsonPropertyName("Genero")]
+        public string Gender { get; set; }
+    }
+}
---
+++ 04_RestWithASPNETUdemy_APIREST-CustomSerialization/RestWithASPNETUdemy/RestWithASPNETUdemy/db/dataset/V4__Populate_Table_Book.sql
@@ -0,0 +1,6 @@
+INSERT INTO `book` VALUES 
+	(1,'Neil Peart','2015-11-06 00:00:00',102.20,'Longe e Distante'),
+	(2,'Henrique Loureiro','2017-07-01 00:00:00',235.25,'C# 7.0 com Visual Studio'),
+	(3,'Augusto Cury','2003-06-23 00:00:00',39.90,'O Vendedor de Sonhos'),
+	(4,'Paul Barry & David Griffiths','2010-12-15 00:00:00',89.90,'Use a Cabeça! Programação'),
+	(5,'Neil Peart','2006-10-05 00:00:00',89.35,'Ghost Rider - A Estrada da Cura');
